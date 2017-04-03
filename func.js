ee5de75ff9723589eb468fb13dc2055573d89b8b
Mechu Simert <msimert@stud.hs-heilbronn.de>
2017-04-03 12:48:12 +0200

    zwei kuchenstücke nebeneinander

--- func.js
+++ func.js
@@ -4,6 +4,7 @@
 window.onload = function() {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
+/*
     ctx.beginPath();
     ctx.arc(95, 50, 40, 0, 2 * Math.PI);
     ctx.fillStyle = "#FF0000";
@@ -31,7 +32,45 @@ window.onload = function() {
     ctx.strokeStyle = "pink";
     ctx.stroke();
     ctx.closePath();
+*/
 
+    // Aufgabe: Zwei Dreiecke (Wuasi zwei Kuchenstücke nebeneinander)
+
+    //1.Dreieck
+    ctx.beginPath();
+    ctx.moveTo(50, 30);
+    ctx.lineTo(200, 30);
+    ctx.strokeStyle="brown";
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.moveTo(200, 30);
+    ctx.lineTo(100, 300);
+    ctx.strokeStyle="brown";
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.moveTo(100, 300);
+    ctx.lineTo(50, 30);
+    ctx.strokeStyle="brown";
+    ctx.stroke();
+    ctx.closePath();
+
+    //2.Dreieck
+    ctx.beginPath();
+    ctx.moveTo(200, 30);
+    ctx.lineTo(350, 120);
+    ctx.strokeStyle="brown";
+    ctx.stroke();
+    ctx.closePath();
+
+    //2.Dreieck
+    ctx.beginPath();
+    ctx.moveTo(350, 120);
+    ctx.lineTo(100, 300);
+    ctx.strokeStyle="brown";
+    ctx.stroke();
+    ctx.closePath();
 }
 
 
