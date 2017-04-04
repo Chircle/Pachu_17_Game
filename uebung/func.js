33b0b48f017aab6f878316250699dadb3babbb08
Mechu Simert <msimert@stud.hs-heilbronn.de>
2017-04-04 23:16:19 +0200

    several changes and exercises on func.js

--- uebung/func.js
+++ uebung/func.js
@@ -41,18 +41,26 @@ window.onload = function() {
     ctx.moveTo(50, 30);
     ctx.lineTo(200, 30);
     ctx.strokeStyle="brown";
+    ctx.lineWidth= 10;
     ctx.stroke();
     ctx.closePath();
 
     ctx.moveTo(200, 30);
     ctx.lineTo(100, 300);
     ctx.strokeStyle="brown";
+    ctx.lineWidth= 10;
     ctx.stroke();
     ctx.closePath();
 
     ctx.moveTo(100, 300);
     ctx.lineTo(50, 30);
     ctx.strokeStyle="brown";
+    ctx.lineWidth= 10;
+
+
+    ctx.fillStyle = "beige";
+    ctx.fill();
+
     ctx.stroke();
     ctx.closePath();
 
@@ -61,16 +69,61 @@ window.onload = function() {
     ctx.moveTo(200, 30);
     ctx.lineTo(350, 120);
     ctx.strokeStyle="brown";
+    ctx.lineWidth= 10;
     ctx.stroke();
     ctx.closePath();
 
-    //2.Dreieck
     ctx.beginPath();
     ctx.moveTo(350, 120);
     ctx.lineTo(100, 300);
     ctx.strokeStyle="brown";
+    ctx.lineWidth= 10;
     ctx.stroke();
     ctx.closePath();
+
+    //Kuchenstueck 1
+    ctx.beginPath();
+    ctx.moveTo(600,30);
+    ctx.lineTo(550, 150);
+    ctx.lineTo(500, 30);
+    ctx.strokeStyle="purple";
+    ctx.fillStyle="pink";
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+
+    //Kuchenstueck 2
+    ctx.beginPath();
+    ctx.moveTo(600,30);
+    ctx.lineTo(700, 90);
+    ctx.lineTo(550, 150);
+    ctx.strokeStyle="blue";
+    ctx.fillStyle="purple";
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+
+    //Kuchenstueck 3
+    ctx.beginPath();
+    ctx.moveTo(700,90);
+    ctx.lineTo(700, 180);
+    ctx.lineTo(550, 150);
+    ctx.strokeStyle="pink";
+    ctx.fillStyle="blue";
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+
+    //Kuchenstueck 4
+    ctx.beginPath();
+    ctx.moveTo(700, 250);
+    ctx.lineTo(700, 180);
+    ctx.lineTo(550, 150);
+    ctx.strokeStyle="purple";
+    ctx.fillStyle="pink";
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
 }
 
 
