f2fd7bee68b600a9c486e728130c6575942ef8a3
Mechu Simert <msimert@stud.hs-heilbronn.de>
2017-04-29 20:24:20 +0200

    color black

---
+++ nicol/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Mechu on 04.04.2017.
+ */
+$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $(this).animate({
+            height: '+=10px'
+        });
+    });
+    $('div').mouseleave(function() {
+        $(this).animate({
+            height: '-=10px'
+        });
+    });
+    $('div').click(function() {
+        $(this).toggle(1000);
+    });
+});
